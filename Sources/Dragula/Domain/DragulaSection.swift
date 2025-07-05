55c9863269775532f4987dd70e9865721b8da64b
Eugene Kovs <smartkovsik@gmail.com>
2025-07-05 20:28:21 -0300

    domain

---
+++ Sources/Dragula/Domain/DragulaSection.swift
@@ -0,0 +1,26 @@
+//
+//  representing.swift
+//  Dragula
+//
+//  Created by Eugene Kovs on 04.07.2025.
+//
+
+import Foundation
+
+/// A protocol representing a section that contains drag-and-droppable items.
+/// Conforming types must provide an array of `DragulaItem`-conforming items.
+///
+/// - Example:
+/// ```swift
+/// struct TaskSection: DragulaSection {
+///     let id: UUID
+///     let title: String
+///     var items: [TaskItem]
+/// }
+/// ```
+public protocol DragulaSection: Identifiable {
+    /// The type of item contained in the section.
+    associatedtype Item: DragulaItem
+    /// The items contained in this section.
+    var items: [Item] { get set }
+}
--- Sources/Dragula/Dragula.swift
+++ Sources/Dragula/Dragula.swift
@@ -5,23 +5,7 @@
 import SwiftUI
 import UIKit
 
-/// A protocol representing a section that contains drag-and-droppable items.
-/// Conforming types must provide an array of `DragulaItem`-conforming items.
-///
-/// - Example:
-/// ```swift
-/// struct TaskSection: DragulaSection {
-///     let id: UUID
-///     let title: String
-///     var items: [TaskItem]
-/// }
-/// ```
-public protocol DragulaSection: Identifiable {
-    /// The type of item contained in the section.
-    associatedtype Item: DragulaItem
-    /// The items contained in this section.
-    var items: [Item] { get set }
-}
+
 
 /// A protocol for individual drag-and-droppable items.
 /// Conforming types can provide an `NSItemProvider` for drag interaction.
